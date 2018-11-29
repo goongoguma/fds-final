e43c0f3ceebde847e2372108d55cb343abbb63e8
goongoguma <wogus7an@gmail.com>
2018-11-29 11:47:55 +0900

    서버로부터 상품 목록 가져와서 표시

--- src/containers/ProductList.js
+++ src/containers/ProductList.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import ProductListView from './ProductListView';
+import api from '../api';
 
 // 서버로부터 데이터를 가져와서 ProductListView에 데이터를 넘겨주는 컴포넌트
 export default class ProductList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      products: [],
+    };
+  }
+
+  // 리액트 개발도구로 받은 데이터 확인해보기
+  async componentDidMount() {
+    const { data: products } = await api.get('/products');
+    this.setState({
+      products,
+      loading: false,
+    });
+  }
+
   render() {
-    const products = [
-      {
-        id: 1,
-        title: '자켓',
-        imgURL: '',
-      },
-      {
-        id: 2,
-        title: '코드',
-        imgURL: '',
-      },
-    ];
-    return <ProductListView products={products} />;
+    const { products } = this.state;
+    // map에서 객체를 바로 반환하기 위해서 ({})를 사용
+    const productsList = products.map(p => ({
+      title: p.title,
+      id: p.id,
+      imgURL: p.mainImgUrl,
+    }));
+    return <ProductListView products={productsList} />;
   }
 }
