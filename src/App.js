b458bd857906a7b205c1fd9da39bdcbb07f1c68a
goongoguma <wogus7an@gmail.com>
2018-11-29 16:28:29 +0900

    로그인 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 import LoginPage from './pages/LoginPage';
+import UserProvider from './contexts/UserContext';
 
 export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <>
-          {/*exact prop이 있을 때 = 주소가 정확히 일치해야만 페이지가 그려진다 */}
-          {/*exact prop이 없을 때 = 주소가 path로 시작하면 페이지가 그려진다 */}
-          <Route exact path="/" component={HomePage} />
-          {/*component의 ProductPage에는 우리가 모르는 props(history, location, match)들이 들어가며 이 정보를 ProductPage 컴포넌트로 넘긴다.   */}
-          <Route path="/product/:productId" component={ProductPage} />
-          <Route path="/login" component={LoginPage} />
-        </>
+        <UserProvider>
+          <>
+            {/*exact prop이 있을 때 = 주소가 정확히 일치해야만 페이지가 그려진다 */}
+            {/*exact prop이 없을 때 = 주소가 path로 시작하면 페이지가 그려진다 */}
+            <Route exact path="/" component={HomePage} />
+            {/*component의 ProductPage에는 우리가 모르는 props(history, location, match)들이 들어가며 이 정보를 ProductPage 컴포넌트로 넘긴다.   */}
+            <Route path="/product/:productId" component={ProductPage} />
+            <Route path="/login" component={LoginPage} />
+          </>
+        </UserProvider>
       </BrowserRouter>
     );
   }
--- src/components/LoginFormView.js
+++ src/components/LoginFormView.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
 
 export default class LoginFormView extends Component {
+  // 제어되는 컴포넌트 만들기
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  handleUsernamerChange(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  handlePasswordChange(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
+  handleLoginButtonClick() {
+    const { onLogin } = this.props;
+    const { username, password } = this.state;
+    onLogin(username, password);
+  }
+
   render() {
+    const { username, password } = this.state;
+
     return (
       <div>
-        <input type="text" />
-        <input type="password" />
-        <button>로그인</button>
+        <input
+          type="text"
+          value={username}
+          name="username"
+          onChange={e => this.handleUsernamerChange(e)}
+        />
+        <input
+          type="password"
+          value={password}
+          name="password"
+          onChange={e => this.handlePasswordChange(e)}
+        />
+        <button onClick={() => this.handleLoginButtonClick()}>로그인</button>
       </div>
     );
   }
